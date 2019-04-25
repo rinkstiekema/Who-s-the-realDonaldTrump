b7d88e008942f3cef4cbf026b2ccac959cae087b
Joost Flick <joostflick@gmail.com>
2019-04-25 11:20:16 +0200

    bugfix + simple points

--- index.js
+++ index.js
@@ -19,84 +19,106 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'public/views'))
 
 app.get('/', (req, res) => {
-	res.render('index')
+  res.render('index')
 })
 
 //twitter keys
 var T = new Twit({
-	consumer_key: 'YFPrRu0ynMMj360eNC2jd91mS',
-	consumer_secret: 'dnwqyvnXONB1ef129N6JRBSsvWZGKNGPK8kCiAIdImlY6dW8RU',
-	access_token: '1118092957402456066-T3NGbV0yeWBjBMzMwdwaPhagjsuw9A',
-	access_token_secret: 'unbxJzG1sZoQFMwKItp5Su5jk9wMwHIH3tPKhrdLEEwlY'
+  consumer_key: 'YFPrRu0ynMMj360eNC2jd91mS',
+  consumer_secret: 'dnwqyvnXONB1ef129N6JRBSsvWZGKNGPK8kCiAIdImlY6dW8RU',
+  access_token: '1118092957402456066-T3NGbV0yeWBjBMzMwdwaPhagjsuw9A',
+  access_token_secret: 'unbxJzG1sZoQFMwKItp5Su5jk9wMwHIH3tPKhrdLEEwlY'
 })
 
 var tweets
 
 //retrieve tweets
 T.get('trends/place', { id: '23424909' }, function(err, data, response) {
-	tweetsWithVolume = data[0].trends.filter(tweet => tweet.tweet_volume)
-	tweets = tweetsWithVolume
+  tweetsWithVolume = data[0].trends.filter(tweet => tweet.tweet_volume)
+  tweets = tweetsWithVolume
 })
 
+//on connection
 io.on('connection', client => {
-	var clientTweets = tweets
-	console.log('new connection')
-	//send 4 random tweets to client
-	var personalTweets = helpers.getRandom(clientTweets, 4)
+  var points = 0
+  console.log('new connection')
+  //get all trends for this client
+  var clientTweets = tweets
+  console.log('Original clientTweets: ', clientTweets)
+  //take 4 of those
+  var personalTweets = helpers.getRandom(clientTweets, 4)
+  //send the name of those 4 to client and remove from all tweets
+  client.emit('tweets', personalTweets.map(tweet => tweet.name))
+  clientTweets = clientTweets.filter(value => !personalTweets.includes(value))
+  console.log('without personal clientTweets: ', clientTweets)
+  //receive answer
+  client.on('answer', answer => {
+    //check if answer is right or wrong
+    var pick = personalTweets.find(tweet => tweet.name == answer)
+    if (!pick) {
+      client.emit('winner')
+      return
+    }
+    console.log('pick + ', pick)
+    var highest = helpers.getMax(personalTweets)
+    //if right
+    if (pick.tweet_volume >= highest) {
+      points += 1
+      client.emit('points', points)
+      //take pick from all tweets and from the 4 options
+      if (clientTweets != []) {
+        clientTweets = clientTweets.filter(value => value != pick)
+      }
+      console.log('clientTweets = ', clientTweets)
+      personalTweets = personalTweets.filter(value => value != pick)
+      console.log('personalTweets = ', personalTweets)
+      //take a new tweet
+      var newTweet = helpers.getRandom(clientTweets, 1)[0]
+      //check if game is over
+      if (newTweet == 'winner') {
+        client.emit('winner')
+        return
+      }
+      console.log('newTweet = ', newTweet)
+      //remove new tweet from the total tweets
+      clientTweets = clientTweets.filter(value => value.name != newTweet.name)
+      console.log('clientTweets = ', personalTweets)
+      //add it to options
+      personalTweets.push(newTweet)
+      console.log('personalTweets = ', personalTweets)
+      //send new tweets to client
+      client.emit('correct', personalTweets.map(tweet => tweet.name))
+      //code to assign points here
+    } else {
+      client.emit('wrong', answer)
+    }
+  })
 
-	client.emit('tweets', personalTweets.map(tweet => tweet.name))
-	client.on('answer', answer => {
-		//check if answer is right
-		var pick = personalTweets.find(tweet => tweet.name == answer)
-		var highest = helpers.getMax(personalTweets)
-		console.log('personalTweets = ', personalTweets)
-		console.log('pick = ', pick)
-		console.log('highest = ', highest)
-		if (pick.tweet_volume >= highest) {
-			clientTweets = clientTweets.filter(value => value != pick)
-			console.log('clientTweets = ', clientTweets)
-			personalTweets = personalTweets.filter(value => value != pick)
-			console.log('personalTweets = ', personalTweets)
-			var newTweet = helpers.getRandom(clientTweets, 1)[0]
-			if (newTweet == 'winner') {
-				client.emit('winner')
-				return
-			}
-			console.log('newTweet = ', newTweet)
-			personalTweets.push(newTweet)
-			console.log('personalTweets = ', personalTweets)
-			client.emit('correct', personalTweets.map(tweet => tweet.name))
-			//code to assign points here
-		} else {
-			client.emit('wrong', answer)
-		}
-	})
-
-	client.on('disconnect', () => console.log('closed connection'))
+  client.on('disconnect', () => console.log('closed connection'))
 })
 
 http.listen(port, () => {
-	console.log(`App running on port ${port}!`)
+  console.log(`App running on port ${port}!`)
 })
 
 var helpers = {
-	//https://stackoverflow.com/questions/19269545/how-to-get-n-no-elements-randomly-from-an-array
-	getRandom: function getRandom(arr, n) {
-		var result = new Array(n),
-			len = arr.length,
-			taken = new Array(len)
-		if (n > len) return ['winner']
-		while (n--) {
-			var x = Math.floor(Math.random() * len)
-			result[n] = arr[x in taken ? taken[x] : x]
-			taken[x] = --len in taken ? taken[len] : len
-		}
-		return result
-	},
-	//get maximum tweet volume
-	getMax: array => {
-		var valuesArray = array.map(option => option.tweet_volume)
-		return Math.max(...valuesArray)
-	},
-	filter: (array, filter) => array.filter(tweet => tweet.name !== filter)
+  //https://stackoverflow.com/questions/19269545/how-to-get-n-no-elements-randomly-from-an-array
+  getRandom: function getRandom(arr, n) {
+    var result = new Array(n),
+      len = arr.length,
+      taken = new Array(len)
+    if (n > len) return ['winner']
+    while (n--) {
+      var x = Math.floor(Math.random() * len)
+      result[n] = arr[x in taken ? taken[x] : x]
+      taken[x] = --len in taken ? taken[len] : len
+    }
+    return result
+  },
+  //get maximum tweet volume
+  getMax: array => {
+    var valuesArray = array.map(option => option.tweet_volume)
+    return Math.max(...valuesArray)
+  },
+  filter: (array, filter) => array.filter(tweet => tweet.name !== filter)
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,36 +1,48 @@
-var options = ['1', '2', '3', '4']
 var socket = io()
 
 socket.on('tweets', function(tweets) {
-	console.log(tweets)
-	fillOptions(tweets)
+  console.log(tweets)
+  fillOptions(tweets)
+})
+
+socket.on('winner', function() {
+  console.log('game over')
 })
 
 socket.on('correct', function(newTweets) {
-	console.log(newTweets)
-	clearOptions()
-	fillOptions(newTweets)
+  console.log(newTweets)
+  clearOptions()
+  fillOptions(newTweets)
 })
 
 socket.on('wrong', function(msg) {
-	console.log(msg)
+  console.log(msg)
+})
+
+socket.on('points', function(points) {
+  updatePoints(points)
+  console.log('points ', points)
 })
 
 function fillOptions(options) {
-	for (let index = 0; index < options.length; index++) {
-		var node = document.createElement('Button')
-		node.onclick = function() {
-			socket.emit('answer', options[index], options)
-		}
-		var textnode = document.createTextNode(options[index])
-		node.appendChild(textnode)
-		document.getElementById('options').appendChild(node)
-	}
+  for (let index = 0; index < options.length; index++) {
+    var node = document.createElement('Button')
+    node.onclick = function() {
+      socket.emit('answer', options[index])
+    }
+    var textnode = document.createTextNode(options[index])
+    node.appendChild(textnode)
+    document.getElementById('options').appendChild(node)
+  }
+}
+
+function updatePoints(points) {
+  document.getElementById('points').innerText = points
 }
 
 function clearOptions() {
-	var options = document.getElementById('options')
-	while (options.firstChild) {
-		options.removeChild(options.firstChild)
-	}
+  var options = document.getElementById('options')
+  while (options.firstChild) {
+    options.removeChild(options.firstChild)
+  }
 }
